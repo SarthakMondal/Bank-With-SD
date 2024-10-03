72367e615c7bc67e802707522e757c6a86526c20
Sarthak Mondal <raceison139@gmail.com>
2024-10-03 10:29:56 +0530

    Adding Codebase to Git

---
+++ src/test/java/in/ebank/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ebank.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
